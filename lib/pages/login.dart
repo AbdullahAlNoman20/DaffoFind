7067e15e8eb81f40834d725bdc4fbbaa4814df46
Abdullah Al Noman <noman15-5387@diu.edu.bd>
2025-03-27 21:59:44 +0600

    Update the Login Page With Firebase

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_one/pages/Register.dart';
-import 'home.dart'; // Your home page after successful login
+import 'home.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -11,31 +11,25 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  bool _isPasswordVisible = false; // State to toggle password visibility
-  final _auth = FirebaseAuth.instance; // Firebase authentication instance
-
+  bool _isPasswordVisible = false;
+  final _auth = FirebaseAuth.instance;
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  String _errorMessage = ''; // To display error messages
+  String? _errorMessage;
 
-  // Function to handle login
   Future<void> _login() async {
+    if (!_formKey.currentState!.validate()) return;
     try {
-      // Attempt to sign in with email and password
       await _auth.signInWithEmailAndPassword(
         email: _emailController.text.trim(),
         password: _passwordController.text.trim(),
       );
-
-      // If successful, navigate to HomePage
       Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const Home()),
-      );
-    } catch (e) {
+        context, MaterialPageRoute(builder: (context) => const Home()));
+    } on FirebaseAuthException catch (e) {
       setState(() {
-        // Update error message to display in case of failure
-        _errorMessage = e.toString();
+        _errorMessage = e.message;
       });
     }
   }
@@ -45,152 +39,111 @@ class _LoginState extends State<Login> {
     return Scaffold(
       backgroundColor: Colors.black,
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 50.0),
+        padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 50.0),
+        child: Form(
+          key: _formKey,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Center(child: Image.asset("images/Banner.jpg", height: 200.0)),
               const SizedBox(height: 30.0),
-              Text(
-                "Welcome To DaffoFind",
-                style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 34.0,
-                    fontWeight: FontWeight.w500),
-              ),
-              Text(
-                "Login",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 45.0,
-                    fontWeight: FontWeight.bold),
-              ),
+              _buildHeader(),
               const SizedBox(height: 40.0),
-              // Email Field
-              Text(
-                "Email",
-                style: TextStyle(
-                    color: Colors.white54,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(height: 5.0),
-              TextField(
-                controller: _emailController,
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white10,
-                  hintText: "Enter your email",
-                  hintStyle: TextStyle(color: Colors.white54),
-                  prefixIcon: Icon(Icons.email, color: Colors.white),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.white24),
-                  ),
-                ),
-              ),
+              _buildTextField("Email", _emailController, Icons.email, false),
               const SizedBox(height: 30.0),
-              // Password Field with Show/Hide Button
-              Text(
-                "Password",
-                style: TextStyle(
-                    color: Colors.white54,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(height: 5.0),
-              TextField(
-                controller: _passwordController,
-                obscureText: !_isPasswordVisible, // Toggle visibility
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white10,
-                  hintText: "Enter your password",
-                  hintStyle: TextStyle(color: Colors.white54),
-                  prefixIcon: Icon(Icons.lock, color: Colors.white),
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _isPasswordVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off,
-                      color: Colors.white70,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _isPasswordVisible =
-                            !_isPasswordVisible; // Toggle state
-                      });
-                    },
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.white24),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10.0),
-              // Forgot Password Button (Aligned Right)
+              _buildTextField("Password", _passwordController, Icons.lock, true),
               Align(
                 alignment: Alignment.centerRight,
                 child: TextButton(
-                  onPressed: () {
-                    // TODO: Add forgot password functionality
-                  },
-                  child: Text(
-                    "Forgot Password?",
-                    style: TextStyle(color: Colors.blueAccent, fontSize: 16),
-                  ),
+                  onPressed: () {},
+                  child: const Text("Forgot Password?", style: TextStyle(color: Colors.blueAccent, fontSize: 16)),
                 ),
               ),
-              const SizedBox(height: 10.0),
-              // Error Message Display
-              if (_errorMessage.isNotEmpty)
-                Text(
-                  _errorMessage,
-                  style: TextStyle(color: Colors.red, fontSize: 16),
+              if (_errorMessage != null)
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: Text(_errorMessage!, style: const TextStyle(color: Colors.red, fontSize: 16)),
                 ),
-              const SizedBox(height: 30.0),
-              // Login Button
-              SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: _login, // Use the login method when button is pressed
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blueAccent,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                  ),
-                  child: Text(
-                    "Login",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
+              _buildLoginButton(),
               const SizedBox(height: 20.0),
-              // Sign Up Button (Navigates to Register Page)
-              Center(
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const Register()),
-                    );
-                  },
-                  child: Text(
-                    "Don't have an account? Sign Up",
-                    style: TextStyle(
-                        color: Colors.blueAccent,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
+              _buildSignUpOption(),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const [
+        Text("Welcome To DaffoFind", style: TextStyle(color: Colors.white70, fontSize: 34.0, fontWeight: FontWeight.w500)),
+        Text("Login", style: TextStyle(color: Colors.white, fontSize: 45.0, fontWeight: FontWeight.bold)),
+      ],
+    );
+  }
+
+  Widget _buildTextField(String label, TextEditingController controller, IconData icon, bool isPassword) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(label, style: const TextStyle(color: Colors.white54, fontSize: 20.0, fontWeight: FontWeight.w500)),
+        const SizedBox(height: 5.0),
+        TextFormField(
+          controller: controller,
+          obscureText: isPassword ? !_isPasswordVisible : false,
+          style: const TextStyle(color: Colors.white),
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white10,
+            hintText: "Enter your $label",
+            hintStyle: const TextStyle(color: Colors.white54),
+            prefixIcon: Icon(icon, color: Colors.white),
+            suffixIcon: isPassword
+                ? IconButton(
+                    icon: Icon(_isPasswordVisible ? Icons.visibility : Icons.visibility_off, color: Colors.white70),
+                    onPressed: () => setState(() => _isPasswordVisible = !_isPasswordVisible),
+                  )
+                : null,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.white24),
+            ),
+          ),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return '$label cannot be empty';
+            }
+            return null;
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLoginButton() {
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: _login,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blueAccent,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        ),
+        child: const Text("Login", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+      ),
+    );
+  }
+
+  Widget _buildSignUpOption() {
+    return Center(
+      child: GestureDetector(
+        onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const Register())),
+        child: const Text(
+          "Don't have an account? Sign Up",
+          style: TextStyle(color: Colors.blueAccent, fontSize: 16, fontWeight: FontWeight.bold),
         ),
       ),
     );
